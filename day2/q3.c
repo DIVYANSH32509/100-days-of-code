d5037f86adcd68573fae01ebb55be7a015b9864d
DIVYANSH32509 <DIVYANSH.32509@stu.upes.ac.in>
2026-09-06 00:18:37 +0530

    day2 added

---
+++ day2/q3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+    int a,b;
+    printf("ENTER THE LENGHT:");
+    scanf("%d",&a);
+    printf("ENTER THE BREATH:");
+    scanf("%d",&b);
+    printf("Perimetre of rectangle: %d\n", 2*(a + b));
+    printf("area of rectangle: %d", a*b);
+    
+}
---
+++ day2/q4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main(){
+
+    float a;
+
+    printf("ENTER THE RADIUS : ");
+    scanf("%f",&a);
+
+    printf("circumference of circle: %f\n",2*3.14*a);
+
+    printf("Area of circle: %f\n",3.14*a*a);
+
+}
