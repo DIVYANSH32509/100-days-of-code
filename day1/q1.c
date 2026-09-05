60bd06ca835665f82af361004fb60bf0b85620a6
DIVYANSH32509 <DIVYANSH.32509@stu.upes.ac.in>
2026-09-06 00:14:46 +0530

    day 1 added

---
+++ day1/q1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+    int a,b,sum;
+    printf("ENTER THE NUMBER:");
+    scanf("%d,%d",&a,&b);
+    sum=a+b;
+    printf("%d",sum);
+
+
+}  
---
+++ day1/q2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+    int a,b; 
+    printf("ENTER THE NUMBER:");
+    scanf("%d,%d",&a,&b);
+    printf("sum=%d\n",a+b);
+    printf("product=%d\n",a*b);
+    printf("subtract=%d\n",a-b);
+    printf("quotient=%d\n",a/b);
+}
